754251cd6def7cefd0dc88d0d42c2d74164e1c9e
Ueverton-oliveira <ueverton.souz@gmail.com>
2024-08-27 12:13:06 -0400

    fix: add comments to recruiter

--- app/models/recruiter.rb
+++ app/models/recruiter.rb
@@ -1,6 +1,8 @@
 class Recruiter < ApplicationRecord
+  # Add method to define and authenticate the passwords
   has_secure_password
 
+  # Validations
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 8 }
