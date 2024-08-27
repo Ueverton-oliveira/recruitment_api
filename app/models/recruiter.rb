2a835b088257b23f481d90c0737a14278bfe780d
Ueverton-oliveira <ueverton.souz@gmail.com>
2024-08-27 10:38:30 -0400

    feat: recruiter

---
+++ app/models/recruiter.rb
@@ -0,0 +1,7 @@
+class Recruiter < ApplicationRecord
+  has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }
+end
